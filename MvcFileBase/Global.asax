28df53cfdad986b0a2156b73c9c7f0277a52daad
Raxid <radoslav.stenke@gmail.com>
2015-03-29 10:33:07 +0200

    MVCFileBaseSprint1
    29-03-15

---
+++ MvcFileBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcFileBasing.MvcApplication" Language="C#" %>
---
+++ MvcFileBase/Models/FileBasing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcFileBasing.Models
+{
+    public class FileBasing
+    {
+        public int Numbers { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class FileBaseDBContext : DbContext
+    {
+        public DbSet<FileBasing> FileBase { get; set; }
+    }
+
+}
