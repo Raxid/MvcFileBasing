67ea02a845709e58114bcaea04d32cbfd1cad962
Raxid <radoslav.stenke@gmail.com>
2015-04-07 09:40:05 +0200

    7.4.15

---
+++ MvcFileBasing/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFileBasing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Informacje o autorze.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FileBase(HttpPostedFileBase file)
+        {
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data"), fileName);
+                file.SaveAs(path);
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult FileBase()
+        {
+            return View();
+        }
+    }
+}
